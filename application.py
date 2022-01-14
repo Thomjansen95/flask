7efa2ed602d11fe74be18a66fcdb2c7f90b31278
Thom <thomjansen95@gmail.com>
2022-01-14 12:36:09 +0000

    added test heartrange lambda files

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import flask
+import boto3
+import logging
+import json
 
 application = Flask(__name__)
 app = application
@@ -7,7 +10,27 @@ app = application
 @app.route('/')
 def index():
     return render_template('index.html')
-    # We are telling flask to look for a file named index in the templates folder
-    #then render it to the user interface. Note that by default flask looks for templates in a folder named templates.
+
+client = boto3.client('lambda')
+                        
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/calculate_heartrate_range", methods=['GET', 'POST'])
+def calculate_heartrate_range():
+    "Endpoint for calculating the heart rate range"
+    if request.method == 'GET':
+        #return the form
+        return render_template('sample_lambda.html')
+    if request.method == 'POST':
+        #return the range        
+        age = int(request.form.get('age'))        
+        payload = {"age":age} 
+        #Invoke a lambda function which calculates the max heart rate and gives the target heart rate range              
+        result = client.invoke(FunctionName="FlaskTestLambda",
+                    InvocationType='RequestResponse',                                      
+                    Payload=json.dumps(payload))
+        range = result['Payload'].read()      
+        api_response = json.loads(range)               
+        return jsonify(api_response)      
+
 if __name__ == '__main__':
     app.run(debug=True)
