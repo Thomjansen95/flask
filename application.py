c4a5263150ad47f3ef45ac4b88e5f6debe8a0dd7
Thom <thomjansen95@gmail.com>
2022-01-14 10:45:23 +0000

    added templates folder and index.html

--- application.py
+++ application.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import flask
+
 application = Flask(__name__)
-
-@application.route("/")
-def hello_world():
-    return "Sup Nerds, via Flask"
-
+app = application
+# Index route
+@app.route('/')
+def index():
+    return render_template('index.html')
+    # We are telling flask to look for a file named index in the templates folder
+    #then render it to the user interface. Note that by default flask looks for templates in a folder named templates.
+if __name__ == '__main__':
+    app.run(debug=True)
